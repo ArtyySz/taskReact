9c165920032dd124637740a9b35ec8656ef32592
Artyy <artemgoncharov236@gmail.com>
2025-12-01 15:40:56 +0300

    finish task

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,1 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import FilterCafes from "./FilterCafes";
 
+export default function CafesTable() {
+    const [data, setData] = useState([]);
+    const [selectedSubway, setSelectedSubway] = useState("All");
+
+    useEffect(() => {
+        axios
+            .get("/cafes")
+            .then((response) => setData(response.data))
+            .catch((error) => console.error("Ошибка:", error));
+    }, []);
+
+    const filteredData =
+        selectedSubway === "All"
+            ? data
+            : data.filter((cafe) => cafe.subwayCode === selectedSubway);
+
+    return (
+        <div className="cafesTable">
+            <FilterCafes onChange={setSelectedSubway} />
+
+            <ul className="cardsList">
+                {filteredData.map((cafe) => (
+                    <li key={cafe.id} className="card">
+                        <img
+                            src={cafe.img || "https://via.placeholder.com/150"}
+                            alt={cafe.name}
+                        />
+                        <h2>{cafe.name}</h2>
+                        <p>{cafe.desc}</p>
+                        <p>{cafe.address}</p>
+                        <p>Subway: {cafe.subwayCode}</p>
+                        <p>{cafe.workTime}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,1 +1,24 @@
+const subways = [
+    { name: "Арбатская", code: "Arbat" },
+    { name: "Александровский сад", code: "Alexanders Garden" },
+    { name: "Московская", code: "Moscow" },
+    { name: "Парк Культуры", code: "Culture" },
+    { name: "Театральная", code: "Theater" },
+];
 
+export default function FilterCafes({ onChange }) {
+    return (
+        <div className="controls">
+            <select name="subway" id="subway" onChange={(e) => onChange(e.target.value)}
+            >
+                <option value="All">Все</option>
+
+                {subways.map((s) => (
+                    <option key={s.code} value={s.code}>
+                        {s.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
